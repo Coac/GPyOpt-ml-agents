e43638d08843e788caad43562faab8e1b25acc5e
Victor Le <levictor3000@hotmail.fr>
2018-07-12 13:34:39 +0200

    Add more hyperparams

--- hyperopt_conf.py
+++ hyperopt_conf.py
@@ -1,4 +1,40 @@
-definition = [{'name': 'learning_rate', 'type': 'continuous', 'domain': (1e-5, 1e-3)}, {'name': 'epsilon', 'type': 'continuous', 'domain': (0.1,0.3)}]
+definition = [{
+  'name': 'learning_rate',
+  'type': 'continuous',
+  'domain': [1e-5, 1e-3]
+}, {
+  'name': 'epsilon',
+  'type': 'continuous',
+  'domain': [0.1, 0.3]
+}, {
+  'name': 'gamma',
+  'type': 'continuous',
+  'domain': [0.8, 0.995]
+}, {
+  'name': 'lambd',
+  'type': 'continuous',
+  'domain': [0.9, 0.95]
+}, {
+  'name': 'num_epoch',
+  'type': 'discrete',
+  'domain': [3, 10]
+}, {
+  'name': 'beta',
+  'type': 'continuous',
+  'domain': [1e-4, 1e-2]
+}, {
+  'name': 'beta',
+  'type': 'continuous',
+  'domain': [1e-4, 1e-2]
+}, {
+  'name': 'num_layers',
+  'type': 'discrete',
+  'domain': [1, 3]
+}, {
+  'name': 'hidden_units',
+  'type': 'discrete',
+  'domain': [32, 64, 128, 256, 512]
+}]
 
 batch_size = 4
 num_cores = 4
